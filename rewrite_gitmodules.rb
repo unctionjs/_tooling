829cfd2bd001bae84c4963985f55dd9699e5b964
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-14 10:14:25 -0700

    Got a new fancy function for iterating over each package

--- rewrite_gitmodules.rb
+++ rewrite_gitmodules.rb
@@ -1,5 +1,5 @@
 require_relative './metadata'
 
-PACKAGES.each do |name|
+each_package do |name|
   File.write(File.join(ENV["HOME"], "Code", "unctionjs", name, ".gitmodules"), "[submodule \"types\"]\n    path = types\n    url = https://github.com/unctionjs/_types.git\n")
 end
